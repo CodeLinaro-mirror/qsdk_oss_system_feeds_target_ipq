32915856ab0eca7be227af3424bf18a5823b5221
Ermine Jose <quic_erminejo@quicinc.com>
2023-04-10 14:16:45 +0530

    target: add ipq53xx board support for rdp468
    Change-Id: Ie464fae2604ec2c5de8d9b8998e4a06b031163ca
    Signed-off-by: Ermine Jose <quic_erminejo@quicinc.com>

--- ipq53xx/image/generic.mk
+++ ipq53xx/image/generic.mk
@@ -17,3 +17,17 @@ define Device/qcom_rdp441
 	IMAGE/sysupgrade.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs | pad-rootfs | append-metadata
 endef
 TARGET_DEVICES += qcom_rdp441
+
+define Device/qcom_rdp468
+	$(call Device/FitImageLzma)
+	DEVICE_VENDOR := Qualcomm Technologies, Inc.
+	DEVICE_MODEL := RDP468
+	DEVICE_VARIANT := AP-MI01.6
+	BOARD_NAME := ap-mi01.6
+	SOC := ipq5332
+	KERNEL_INSTALL := 1
+	KERNEL_SIZE := 6096k
+	IMAGE_SIZE := 25344k
+	IMAGE/sysupgrade.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs | pad-rootfs | append-metadata
+endef
+TARGET_DEVICES += qcom_rdp468
