c0f5ebcef2ebda4f2ef03efd28d477938d08950b
Ermine Jose <quic_erminejo@quicinc.com>
2023-04-26 11:36:18 +0530

    ipq53xx: Add support for ipq53xx_32 with Linux v6.1
    This change adds support for ipq53xx_32 SoC with Linux v6.1
    Added single itb image support
    Default profile is selected by DEVICE_NAME
    Added config-6.1 kernel conifgs
    
    The supported Board Names:
    	RDP441
    	RDP468
    
    The ipq53xx target has two subtargets
     1) subtarget 'generic' denotes 64bit
     2) subtarget 'ipq53xx_32' denotes 32bit
    
    Change-Id: Icd81e1c01d81fab6350b5f3c88735f184c91099c
    Signed-off-by: Ermine Jose <quic_erminejo@quicinc.com>

---
+++ ipq53xx/ipq53xx_32/target.mk
@@ -0,0 +1,9 @@
+
+ARCH:=arm
+SUBTARGET:=ipq53xx_32
+BOARDNAME:=QTI IPQ53xx(32bit) based boards
+CPU_TYPE:=cortex-a7
+
+define Target/Description
+	Build firmware image for IPQ53xx SoC devices.
+endef
