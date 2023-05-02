1454e2240a03fe9aebac97c29fe74f7975867756
Ram Chandra Jangir <quic_rjangir@quicinc.com>
2023-05-02 17:40:20 +0530

    ipq: Enable uboot-2016 for 32bit builds
    Change-Id: I62f94312e4f3c57c2a53b63f53cd5c8e1d16b4f3
    Signed-off-by: Ram Chandra Jangir <quic_rjangir@quicinc.com>

--- ipq53xx/ipq53xx_32/target.mk
+++ ipq53xx/ipq53xx_32/target.mk
@@ -4,6 +4,9 @@ SUBTARGET:=ipq53xx_32
 BOARDNAME:=QTI IPQ53xx(32bit) based boards
 CPU_TYPE:=cortex-a7
 
+DEFAULT_PACKAGES += \
+	uboot-2016-ipq5332
+
 define Target/Description
 	Build firmware image for IPQ53xx SoC devices.
 endef
