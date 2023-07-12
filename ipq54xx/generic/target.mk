ece490f4b826280d084460f74ff1d16d02438a12
Ermine Jose <quic_erminejo@quicinc.com>
2023-07-12 13:12:17 +0530

    ipq54xx: add initial openwrt target support
    Change-Id: I18bcb1c40ec80f23412ee322ccfc2171652ddc58
    Signed-off-by: Ermine Jose <quic_erminejo@quicinc.com>

---
+++ ipq54xx/generic/target.mk
@@ -0,0 +1,8 @@
+SUBTARGET:=generic
+BOARDNAME:=QTI IPQ54xx(64bit) based boards
+CPU_TYPE:=cortex-a55
+
+define Target/Description
+	Build images for ipq54xx 64 bit system.
+endef
+
---
+++ ipq54xx/ipq54xx_32/target.mk
@@ -0,0 +1,10 @@
+
+ARCH:=arm
+SUBTARGET:=ipq54xx_32
+BOARDNAME:=QTI IPQ54xx(32bit) based boards
+CPU_TYPE:=cortex-a7
+
+define Target/Description
+	Build firmware image for IPQ54xx SoC devices.
+endef
+
