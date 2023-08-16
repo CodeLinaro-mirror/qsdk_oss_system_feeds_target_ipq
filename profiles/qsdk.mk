ec135abe9c30c666a380e28ed69cdc00b9e27b8b
Ermine Jose <quic_erminejo@quicinc.com>
2023-08-16 23:44:14 +0530

    qsdk.mk: enable hostapd-utils
    Change-Id: Ib1a3c6ec240069a1c7efb842bd265ff9231d27f4
    Signed-off-by: Ermine Jose <quic_erminejo@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -38,7 +38,7 @@ SWITCH_SSDK_PKGS:= kmod-qca-ssdk-hnat kmod-qca-ssdk-nohnat qca-ssdk-shell swconf
 
 MACSEC_OPEN_PKGS:= kmod-qca-nss-macsec -wpa-supplicant-macsec -hostapd-macsec
 
-WIFI_OPEN_PKGS:= kmod-ath12k kmod-ath11k wpad-mesh -hostapd-utils \
+WIFI_OPEN_PKGS:= kmod-ath12k kmod-ath11k wpad-mesh hostapd-utils \
 	sigma-dut-open wpa-cli qca-wifi-scripts
 #	sigma-dut-open wpa-cli qcmbr-netlink iwinfo \
 #	athtestcmd athtestcmd-lith-nl -qca-whc-lbd -qca-whc-init -libhyficommon qca-wifi-scripts
