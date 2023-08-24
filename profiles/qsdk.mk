49f1bb9841b1564b785765d0ca02fc682673dcf7
Hariharan K <quic_harihk@quicinc.com>
2023-08-24 19:06:45 +0530

    qsdk.mk: Enable qti-softsku-license-loader-libs package
    Change-Id: I1522a413b86f84fa9bc05c0c54eedb66675ada39
    Signed-off-by: Hariharan K <quic_harihk@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -100,7 +100,7 @@ TEST_TOOLS:=ethtool i2c-tools tcpdump
 UTILS:=file luci-app-samba4 rng-tools profilerd
 
 COREBSP_UTILS:=pm-utils wififw_mount_script qca-thermald qca-qmi-framework -qca-time-services \
-	qca-wlanfw-upgrade qti-license-pfm dashboard
+	qca-wlanfw-upgrade qti-license-pfm dashboard qti-softsku-license-loader-libs
 
 FAILSAFE:= kmod-bootconfig
 DEFAULT_PACKAGES += -dnsmasq
