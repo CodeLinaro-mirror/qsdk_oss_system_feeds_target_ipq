80c0ecaf3b28b646edaaf8e11b57254d1cde0c9e
Ermine Jose <quic_erminejo@quicinc.com>
2023-09-21 19:56:06 +0530

    qsdk.mk: enable tc-full instead of tc-tiny in QOS
    Change-Id: I0211c62be1607cec092d4d1ce8f37ef747f1fbfa
    Signed-off-by: Ermine Jose <quic_erminejo@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -140,7 +140,7 @@ BLUETOPIA:=bluetopia
 
 ZIGBEE:=zigbee_efr32
 
-QOS:=tc-tiny kmod-sched kmod-sched-core kmod-sched-connmark kmod-ifb iptables \
+QOS:=tc-full kmod-sched kmod-sched-core kmod-sched-connmark kmod-ifb iptables \
 	iptables-mod-filter iptables-mod-ipopt iptables-mod-conntrack-extra
 
 MAP_PKGS:=map 464xlat tayga
