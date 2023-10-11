f062c199f9ef0ff2d5ef44342c09ec6842e3e13b
Tushar Ganatra <quic_tganatra@quicinc.com>
2023-10-11 16:21:21 +0530

    [qsdk] : enable ppe-qdisc package
    Change-Id: I4d25d5eb858449e7dacadad8abde9a80e737b97d
    Signed-off-by: Tushar Ganatra <quic_tganatra@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -20,6 +20,7 @@ NSS_PPE:= kmod-qca-nss-ppe \
 	kmod-qca-nss-ppe-vxlanmgr \
 	-kmod-qca-nss-ppe-mapt \
 	kmod-qca-nss-ppe-rule \
+	kmod-qca-nss-ppe-qdisc \
 	kmod-qca-nss-ppe-mirror-test
 
 NSS_CRYPTO:= -kmod-qca-nss-crypto -kmod-qca-nss-cfi-cryptoapi -kmod-qca-nss-cfi-ocf -kmod-crypto-ocf \
