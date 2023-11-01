8e0de3683ad1cd511534fb338870fef4f316fa63
Madhurya Kandamuru <quic_madhurya@quicinc.com>
2023-11-01 16:02:12 +0530

    target: ipq53xx board support for qcom,ipq5332-ap-mi01.2-c2
    Change-Id: I639114dd3d0b6f85d10445261caa83e81228d5f2
    Signed-off-by: Madhurya Kandamuru <quic_madhurya@quicinc.com>

--- ipq53xx/base-files/lib/upgrade/platform.sh
+++ ipq53xx/base-files/lib/upgrade/platform.sh
@@ -564,6 +564,7 @@ platform_do_upgrade() {
 	case "$board" in
 	qcom,devsoc-ap-emulation |\
 	qcom,ipq5332-ap-mi01.2 |\
+	qcom,ipq5332-ap-mi01.2-c2 |\
 	qcom,ipq5332-ap-mi01.2-qcn9160-c1 |\
 	qcom,ipq5332-ap-mi01.3 |\
 	qcom,ipq5332-ap-mi01.3-c2 |\
