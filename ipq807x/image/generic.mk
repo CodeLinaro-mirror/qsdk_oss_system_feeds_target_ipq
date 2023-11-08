3c2e00cc1b5966efc224abdec223fc7d56942377
Ermine Jose <quic_erminejo@quicinc.com>
2023-11-08 15:26:51 +0530

    ipq807x: Add target support with OpenWRT 23.05 and Kernel v5.4
    This change adds support for ipq807x SoC with Linux v5.4
    Added Multi itb image support
    Default profile is selected by DEVICE_NAME
    Added config-5.4 kernel configs
    
    The ipq807x target has two subtargets
     1) subtarget 'generic' denotes 64bit
     2) subtarget 'ipq807x_32' denotes 32bit
    
    Signed-off-by: Ermine Jose <quic_erminejo@quicinc.com>
    Change-Id: Icf37de2b70004b26bb0bff876a2e582ac94a47f0

---
+++ ipq807x/image/generic.mk
@@ -0,0 +1,18 @@
+
+DEVICE_VARS += NETGEAR_BOARD_ID NETGEAR_HW_ID
+DEVICE_VARS += RAS_BOARD RAS_ROOTFS_SIZE RAS_VERSION
+DEVICE_VARS += WRGG_DEVNAME WRGG_SIGNATURE
+
+define Device/qcom_hkxx
+        $(call Device/MultiDTBFitImage)
+	DEVICE_VENDOR := Qualcomm Technologies, Inc.
+	DEVICE_MODEL := AP-HKXX
+	DEVICE_VARIANT :=
+	BOARD_NAME := ap-hkxx
+	SOC := ipq8074
+	KERNEL_INSTALL := 1
+	KERNEL_SIZE := $(if $(CONFIG_DEBUG),7680k,6500k)
+	IMAGE_SIZE := 25344k
+	IMAGE/sysupgrade.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs | pad-rootfs | append-metadata
+endef
+TARGET_DEVICES += qcom_hkxx
---
+++ ipq807x/ipq807x_32/target.mk
@@ -0,0 +1,13 @@
+
+ARCH:=arm
+SUBTARGET:=ipq807x_32
+BOARDNAME:=QTI IPQ807x(32bit) based boards
+CPU_TYPE:=cortex-a7
+
+define Target/Description
+	Build firmware image for IPQ807x SoC devices.
+endef
+
+DEFAULT_PACKAGES += \
+	uboot-2016-ipq807x uboot-2016-ipq807x_tiny lk-ipq807x \
+	fwupgrade-tools
