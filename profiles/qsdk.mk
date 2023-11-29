82bfae439657b67c8a7210f51e05c12392a46e90
Urvishkumar Pandya <quic_urvishku@quicinc.com>
2023-11-29 11:38:26 +0530

    qsdk.mk: Enable wifi package for linux-6.1
    Enable wrapd, spectral, wapid and icm package for
    ginger branch
    
    Change-Id: I0de15e051632798d3a9404784484245cb6a898d1
    Signed-off-by: Urvishkumar Pandya <quic_urvishku@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -56,7 +56,8 @@ WIFI_PKGS_6_1:=kmod-qca-wifi-unified-profile \
 	qca-wifi-hk-fw-hw1-10.4-asic \
 	qca-cfg80211 qca-hostap qca-hapd-supp qca-hostapd-cli qca-wpa-supplicant \
 	qca-wpa-cli qca-cfg80211tool athdiag \
-	qca-wifi-scripts sigma-dut
+	qca-wifi-scripts sigma-dut qca-wrapd \
+	qca-spectral qca-icm qca-wapid
 
 WIFI_PKGS:=kmod-qca-wifi-unified-profile \
 	qca-hostap qca-hostapd-cli qca-wpa-supplicant \
