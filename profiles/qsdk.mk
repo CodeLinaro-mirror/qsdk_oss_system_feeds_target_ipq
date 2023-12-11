2a9a686b79efb6823a7750494183761f82e9dfac
Nagarajan Maran <quic_nmaran@quicinc.com>
2023-12-11 18:06:27 +0530

    qsdk.mk: Enable telemetry_agent module for kernel-6.x
    The telemetry_agent LKM which was enabled in kernel-5.x is not enabled
    in kernel-6.x. This change adds build support to enable
    telemetry_agent.
    
    Change-Id: Ic8908aa6fd9d77bd2b456f2506b8c2e5a955a298
    Signed-off-by: Nagarajan Maran <quic_nmaran@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -104,7 +104,7 @@ MACSEC_OPEN_PKGS:= kmod-qca-nss-macsec -wpa-supplicant-macsec -hostapd-macsec
 NSS_L2TP:= kmod-l2tp kmod-l2tp-ip kmod-l2tp-eth
 
 WIFI_OPEN_PKGS:= kmod-ath12k kmod-ath11k wpad-mesh hostapd-utils \
-	control-app-open sigma-dut-open wpa-cli qca-wifi-scripts cnssdiag myftm
+	control-app-open sigma-dut-open wpa-cli qca-wifi-scripts cnssdiag myftm kmod-telemetry-agent
 #	sigma-dut-open wpa-cli qcmbr-netlink iwinfo \
 #	athtestcmd athtestcmd-lith-nl -qca-whc-lbd -qca-whc-init -libhyficommon qca-wifi-scripts -kmod-telemetry-agent
 
