447f97715239baafc922937b07ec7497c8ac6454
KM <quic_suresure@quicinc.com>
2024-02-19 12:38:32 +0530

    Enabling ezmesh on 256MB profile
    Change-Id: I65d87a40fbb361f5c9c3db06da8a2f50a242e56d
    Signed-off-by: KM <quic_suresure@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -133,7 +133,7 @@ WIFI_PKGS_256MB:=kmod-qca-wifi-lowmem-profile \
 	qca-wpa-cli qca-cfg80211tool qca-wifi-scripts \
 	-qca-wpc sigma-dut qca-wrapd qca-wapid -qca-acfg \
 	-qca-iface-mgr qca-icm qca-cfg80211 athdiag -qca-cnss-daemon \
-	-athtestcmd-lith -whc-mesh -whc-ui myftm
+	-athtestcmd-lith whc-mesh whc-ui myftm
 
 WIFI_PKGS_16M:=kmod-qca-wifi-flash_16mb-profile \
 	qca-hostap qca-hostapd-cli -qca-hapd-supp qca-wpa-supplicant \
