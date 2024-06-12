005e754480b57bcf1d2bf617803c2e875ff1674a
Atchaya R <quic_ratchaya@quicinc.com>
2024-06-12 10:59:05 +0530

    qsdk.mk: enable hota-driver for Cov profile
    Signed-off-by: Atchaya R <quic_ratchaya@quicinc.com>
    Change-Id: Ic2b5eb1d657c2116b88e072d505c259ce8531648
    (cherry picked from commit c8dbd822e16cfea2de2cf1857119d250975fe4d4)

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -327,7 +327,7 @@ define Profile/QSDK_Cov
 		$(HW_CRYPTO) $(IPSEC) $(MINIDUMP) $(QOS) $(HYFI) $(NSS_USERSPACE_OSS)\
 		$(QCA_MAD) $(QCA_EZMESH) $(OPENVPN) kmod-macvlan \
 		kmod-qca-hyfi-bridge $(NSS_NSM) $(SAL_QOS) $(WIFI_PKGS) $(NPT66) \
-		$(NSS_FLS) $(NSS_L2TP) $(EMESH_SP) $(WIFI_FW_PKGS)
+		$(NSS_FLS) $(NSS_L2TP) $(EMESH_SP) $(WIFI_FW_PKGS) kmod-hota-driver
 endef
 
 #		$(NSS_UDP_ST) $(NSS_MACSEC)
