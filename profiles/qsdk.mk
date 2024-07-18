71df4f65609586074bae7c977d84ff79ea08d752
Ram Kumar D <quic_ramd@quicinc.com>
2024-07-18 10:01:39 +0530

    [profiles]: Enable minidump support for open profile
    Change-Id: Id8ae36a1ceac3c80011264f04664f40e9e27f030
    Signed-off-by: Ram Kumar D <quic_ramd@quicinc.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -381,7 +381,7 @@ define Profile/QSDK_Open
 		$(SWITCH_SSDK_NOHNAT_PKGS) $(SWITCH_SSDK_PKGS) $(KPI) $(IGMPSNOOPING_RSTP) $(MAP_PKGS) \
 		$(WIFI_OPEN_PKGS) -lacpd -qca-thermald $(UTILS) $(EXTRA_NETWORKING) \
 		$(USB_ETHERNET) $(NSS_COMMON) $(NSS_STANDARD) $(NSS_MESH) $(EMESH_SP) $(NPT66)\
-		$(NSS_NSM) $(SAL_QOS) $(IPSEC) $(NSS_CRYPTO) $(QOS) -lacpd $(MACSEC_OPEN_PKGS) \
+		$(NSS_NSM) $(SAL_QOS) $(IPSEC) $(MINIDUMP) $(NSS_CRYPTO) $(QOS) -lacpd $(MACSEC_OPEN_PKGS) \
 		-qca-cnss-daemon qca-wifi-hk-fw-hw1-10.4-asic athdiag qrtr ath11k-fwtest ath11k-qdss \
 		-qapp-store libtirpc cfr_tools kmod-qca-ovsmgr -qca-mcs-apps \
 		 $(NSS_FLOWID) $(NSS_FLS) kmod-macvlan $(NSS_L2TP) wpad-mesh-openssl \
