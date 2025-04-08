2dcfc88e4520a60f6c09a348a4922f2e29c9cf97
Madhurya Kandamuru <quic_madhurya@quicinc.com>
2025-04-08 10:46:04 +0530

    Add initial OpenWrt target support for IPQ5200
    Change-Id: Ib0c951c5dee00351e23654eed4b077e12af7bc01
    Signed-off-by: Madhurya Kandamuru <quic_madhurya@quicinc.com>

---
+++ ipq52xx/ipq52xx_32/target.mk
@@ -0,0 +1,9 @@
+
+ARCH:=arm
+SUBTARGET:=ipq52xx_32
+BOARDNAME:=QTI IPQ52xx(32bit) based boards
+CPU_TYPE:=cortex-a7
+
+define Target/Description
+	Build firmware image for IPQ52xx SoC devices.
+endef
