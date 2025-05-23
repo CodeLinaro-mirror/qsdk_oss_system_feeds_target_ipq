c8b0ca6bd22b100c50be4fd1492587bc36651f0c
Madhurya Kandamuru <madhurya@qti.qualcomm.com>
2025-05-23 10:52:13 +0530

    Add initial OpenWrt target support for IPQ9679
    Change-Id: I6564ad028ddc895c1f480f5604dbd11abccd4889
    Signed-off-by: Madhurya Kandamuru <madhurya@qti.qualcomm.com>

---
+++ ipq96xx/image/generic.mk
@@ -0,0 +1,28 @@
+
+define Device/qcom_jhxx
+        $(call Device/MultiDTBFitImage)
+        DEVICE_VENDOR := Qualcomm Technologies, Inc.
+        DEVICE_MODEL := AP-JHXX
+        DEVICE_VARIANT :=
+        BOARD_NAME := ap-jhxx
+        SOC := ipq9679
+        KERNEL_INSTALL := 1
+        KERNEL_SIZE := $(if $(CONFIG_DEBUG),9216k,6500k)
+        IMAGE_SIZE := 25344k
+        IMAGE/sysupgrade.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs | pad-rootfs | append-metadata
+endef
+TARGET_DEVICES += qcom_jhxx
+
+define Device/emulation
+	$(call Device/FitImageLzma)
+	DEVICE_VENDOR := Qualcomm Technologies, Inc.
+	DEVICE_MODEL :=
+	DEVICE_VARIANT :=
+	BOARD_NAME :=
+	BUILD_DTS_ipq9679-emulation := 1
+	SOC := ipq9679
+	KERNEL_INSTALL := 1
+	KERNEL_SIZE := $(if $(CONFIG_DEBUG),8680k,6500k)
+	IMAGE_SIZE := 25344k
+	IMAGE/sysupgrade.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs | pad-rootfs | append-metadata
+endef
