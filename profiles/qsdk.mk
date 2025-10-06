70b197217b283414fd9001de1638728e0bd69426
Deepak Kumar <deepaku@qti.qualcomm.com>
2025-10-06 15:55:56 +0530

    Add perf package for open profile
    Change-Id: Icc48e299acde86f84eb8e4e2e839c9de84eefed4
    Signed-off-by: Deepak Kumar <deepaku@qti.qualcomm.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -205,7 +205,7 @@ define Profile/QSDK_Open
 		-qca-cnss-daemon qca-wifi-hk-fw-hw1-10.4-asic athdiag qrtr ath11k-fwtest ath11k-qdss \
 		-qapp-store libtirpc cfr_tools \
 		wpad-mesh-openssl \
-		iperf3 \
+		iperf3 libunwind perf \
 		$(PRPLMESH_DEP_PKGS) \
 		$(NSS_OPEN)
 endef
