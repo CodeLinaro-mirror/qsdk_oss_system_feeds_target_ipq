8ddd897b0d271e57015404173e590c5db60cd6e5
Swati Singh <swasing@qti.qualcomm.com>
2026-05-18 14:11:56 +0530

    [linux][feeds] Enable DS offload for 256_Open profile
    Enable Wi-Fi NSS plugins, eMesh, and ath client modules
    to support VPS (ath client) registration.
    This facilitates callback registration by Wi-Fi plugins
    to get the valid DS node ID and enables classifier
    registration through eMesh modules for mark metadata for
    MLO assist.
    
    Change-Id: I24c226928d2a92736d8df473d7655b034cfa0d44
    Signed-off-by: Swati Singh <swasing@qti.qualcomm.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -242,7 +242,8 @@ define Profile/QSDK_256Open
 		$(FTM) $(DIAG) -qca-thermald \
 		-kmod-usb-phy-ipq807x kmod-qca-hyfi-bridge \
 		$(NSS_256) kmod-bonding kmod-qca-debug-uio\
-		$(WIFI_OPEN_PKGS) $(WIFI_FW_PKGS) $(MACSEC_OPEN_PKGS) \
+		$(EMESH_SP) $(ATH_CLIENT) \
+		$(WIFI_OPEN_PKGS) $(WIFI_OPEN_NSS_PLGN_PKGS) $(WIFI_FW_PKGS) $(MACSEC_OPEN_PKGS) \
 		$(OPTEE_CLIENT) $(PON_PKGS) $(ERP)
 endef
 
