60ff45311be419867bdaca4ab22671bf8bc2d36b
Naveen Raj Ranganathan <naver@qti.qualcomm.com>
2026-05-23 10:34:01 +0530

    Enabling mem-profile package in qsdk.mk
    Change-Id: I031c187bacba807ade5b3a17f3b2680e78d13bee
    Signed-off-by: Naveen Raj Ranganathan <naver@qti.qualcomm.com>

--- profiles/qsdk.mk
+++ profiles/qsdk.mk
@@ -52,7 +52,7 @@ WIFI_FW_PKGS:=qca-wifi-wkk-fw-hw1-asic
 
 OPENWRT_STANDARD:= luci lua openssl-util zram-swap
 
-OPENWRT_BASIC:= wifi-scripts memory-dashboard
+OPENWRT_BASIC:= wifi-scripts memory-dashboard mem-profile
 
 OPENWRT_256MB:= pm-utils wififw_mount_script qca-thermald qti-license-pfm
 
