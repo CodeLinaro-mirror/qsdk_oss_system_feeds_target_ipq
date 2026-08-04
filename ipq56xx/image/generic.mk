0892b2457c43d8572a0b8bcaf40194d9ce1de88d
Haritha Muthukumar <hmuthuku@qti.qualcomm.com>
2026-08-04 09:44:59 +0530

    ipq56xx: add qcom_emulation-fbc device for ITB generation
    Add a Device/qcom_emulation-fbc definition mirroring the ipq96xx
    pattern so an ITB image is generated for the IPQ5610 emulation FBC
    board.
    
    Change-Id: Id35906997dda4a0e7e94b099fac400048e92fdf7

--- ipq56xx/image/generic.mk
+++ ipq56xx/image/generic.mk
@@ -25,3 +25,19 @@ define Device/emulation
 	IMAGE/sysupgrade.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs | pad-rootfs | append-metadata
 endef
 TARGET_DEVICES += emulation
+
+define Device/qcom_emulation-fbc
+	$(call Device/FitImageLzma)
+	DEVICE_VENDOR := Qualcomm Technologies, Inc.
+	DEVICE_MODEL :=
+	DEVICE_VARIANT :=
+	BOARD_NAME :=
+	DEVICE_DTS := ipq5610-emulation-fbc
+	BUILD_DTS_ipq5610-emulation-fbc := 1
+	SOC := ipq5610
+	KERNEL_INSTALL := 1
+	KERNEL_SIZE := $(if $(CONFIG_DEBUG),8680k,6500k)
+	IMAGE_SIZE := 25344k
+	IMAGE/sysupgrade.bin := append-kernel | pad-to $$$$(KERNEL_SIZE) | append-rootfs | pad-rootfs | append-metadata
+endef
+TARGET_DEVICES += qcom_emulation-fbc
